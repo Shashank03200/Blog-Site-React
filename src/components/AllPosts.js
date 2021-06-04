2084920cc8b830eea163db741e79316e21a67542
Shashank03200 <shashank032000@gmail.com>
2021-06-05 00:54:15 +0530

    Async/Await

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -9,31 +9,30 @@ import AuthContext from '../store/auth-context';
 
 const AllPosts = () => {
 
-    let posts = []
+    const [posts, setPosts] = useSTate([]);
     const { userId } = useContext(AuthContext);
 
 
 
-    useEffect(() => {
-        fetch('https://blog-app-8981b-default-rtdb.firebaseio.com/posts.json')
-            .then(response => response.json())
-            .then(posts => {
-                const initialPosts = [];
-                // data is the object posts
-                for (const post in posts) {
+    useEffect(async function getAllPosts() {
+        const response = await fetch('https://blog-app-8981b-default-rtdb.firebaseio.com/posts.json');
+        const posts = await response.json();
 
-                    if (posts[post].userId === userId) {
-                        const postObject = {
-                            postId: post,
-                            postData: posts[post]
-                        }
-                        initialPosts.push(postObject);
-                    }
+        const initialPosts = [];
+        // data is the object posts
+        for (const post in posts) {
+
+            if (posts[post].userId === userId) {
+                const postObject = {
+                    postId: post,
+                    postData: posts[post]
                 }
-                console.log(initialPosts)
-                posts = initialPosts;
-            })
-    }, [posts])
+                initialPosts.push(postObject);
+            }
+        }
+        getAllPosts()
+    }, [])
+
 
 
     let postList = <p>Loading...</p>
